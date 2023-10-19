cee6658cbc30a0a2007363d6d54d3ab034c6572f
gekicchi <oyarzunm18@gmail.com>
2023-10-18 22:14:59 -0300

    primeros 2 ejercicios

---
+++ lab5_ejercicio2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int entero = 573;
+	float flotante = 21.88;
+	double doble = 98.555166;
+	char caracter[10] = "caracter";
+	string cadena = "palabritas string";
+	bool booleano = false;
+	
+	cout << "int: " << entero;
+	cout << "float: " << flotante;
+	cout << "double: " << doble;
+	cout << "char: " << caracter;
+	cout << "string: " << cadena;
+	cout << "bool: " << booleano;
+	
+	return 0;
+}
